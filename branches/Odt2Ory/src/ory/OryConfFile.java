d013aa9c1ba3b989bf1baa6accbb05dcd70289fb
izar00@gmail.com <izar00@gmail.com@aeb61573-d5a9-0769-0c8e-f8bf28dd2c43>
2011-06-14 12:28:40 +0000

    added minimal gui + auto uid in conf

--- branches/Odt2Ory/src/ory/OryConfFile.java
+++ branches/Odt2Ory/src/ory/OryConfFile.java
@@ -2,13 +2,26 @@ package ory;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 
 import org.apache.commons.io.FileUtils;
 
+/**
+ * class for creating a .conf (configuration) file
+ * for an orayta book.
+ * @author izar00@gmail.com
+ *
+ */
 public class OryConfFile {
 	private Filename filename;
 	private StringBuffer text;
+	private final int UID_MIN = 0;
+	private final int UID_MAX = 40000;
 	
+	/**
+	 * 
+	 * @param file - the Filename object for the ory file for which this conf is meant.
+	 */
 	OryConfFile (Filename file) {
 		
 		String path = file.getFullPath();
@@ -17,8 +30,27 @@ public class OryConfFile {
 		text = new StringBuffer();
 		
 		filename = new Filename (path, baseName, extension);
+		
+		setAutoUid();
 	}
 	
+	/**
+	 * creates a unique id entry
+	 * with a number generated from the path
+	 * of this conf file.
+	 * NOTICE: you may get a negative uid.
+	 */
+	private void setAutoUid() {
+		String str = (filename.getFilename() + new Date()); // using date to decrease probability of books with the same uid.
+		int num = str.hashCode();
+		
+		//make sure our number isn't in the range that is in use.
+		if (num > UID_MIN && num < UID_MAX)
+			num += UID_MAX;
+		
+		setUID(num);
+	}
+
 	void addEntry (String label, String value){
 		String str = new String (label + "=" + value + "\n");
 //		System.out.println("to add: " + str);
@@ -36,6 +68,23 @@ public class OryConfFile {
 	
 	public void setUID(String uid){
 		addEntry("UniqueId", uid);
+	}
+
+	public void setUID(int num) {
+		String uid = Integer.toString(num);
+		setUID(uid);
+	}
+
+	public Filename getFilename() {
+		return filename;
+	}
+
+	public void setFilename(Filename filename) {
+		this.filename = filename;
+	}
+
+	public String getText() {
+		return text.toString();
 	}
 
 	public void save() throws IOException {
