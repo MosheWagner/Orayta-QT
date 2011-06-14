d733ef836041e4389ea242f8d83312420a89316b
izar00@gmail.com <izar00@gmail.com@aeb61573-d5a9-0769-0c8e-f8bf28dd2c43>
2011-06-14 15:57:41 +0000

    giur gui updated.

--- branches/Odt2Ory/src/ory/Gui.java
+++ branches/Odt2Ory/src/ory/Gui.java
@@ -1,13 +1,16 @@
 package ory;
 
+import java.io.FileNotFoundException;
+
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileFilter;
 
 public class Gui {
 	
 	private String inputFilename;
 
-	Gui () {
+	Gui () throws Exception {
 		
 //		String defaultPath = OryFile.getOraytaDir();
 		String defaultPath = ".";
@@ -18,19 +21,45 @@ public class Gui {
 //		fileChooser.setFileFilter(filter ); //TODO: create file filter.
 		
 		fileChooser.setSelectedFile(null);
-		int option = fileChooser.showOpenDialog(null); 
+		int option = fileChooser.showOpenDialog(null);
+		
 		if (option != JFileChooser.APPROVE_OPTION) {
 			return; //TODO: create an exit() method?
 		}
-		else {
-		inputFilename = fileChooser.getSelectedFile().getAbsolutePath();
+		
+		try {
+			inputFilename = fileChooser.getSelectedFile().getAbsolutePath();
+			new Odt2Ory(inputFilename);
+		} catch (FileNotFoundException e) {
+			/* TODO: handle exception
+			 * should look like this: */
+			String message = ("File not found" + "\n" + "הקובץ לא נמצא" + "\n");
+			JOptionPane.showMessageDialog(null,message + e);
+			return;
+			
+		} catch (Exception e) {
+			String message = ("an error occured" + "\n" + "אירעה שגיאה" + "\n");
+			JOptionPane.showMessageDialog(null,message + e);
+			return;
+			
 		}
-				
+		
+		success();		
 	}
 	
 	
 	
 	
+	private void success() {
+		String message = ("oporation completed successfully" + "\n" + "הפעולה הסתיימה בהצלחה" + "\n");
+		JOptionPane.showMessageDialog(null,message);
+		return;
+		
+	}
+
+
+
+
 	public String getFilename() {
 		return inputFilename;
 	}
