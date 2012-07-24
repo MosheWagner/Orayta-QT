ef9ed6bd330a0601eac6af6138f3ee749c37f23b
izar00@gmail.com <izar00@gmail.com@aeb61573-d5a9-0769-0c8e-f8bf28dd2c43>
2012-07-24 15:24:11 +0000

    עדכון לעץ ספרים מקורי. עוד כמה ספרים שמשום מה היו חסרים

---
+++ BooksSrc/undo.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+
+
+
+
+
+
+
+for f in `find . -iname "*.obk"`
+do
+    echo "deflating: "$f
+    base=`echo $f | sed 's/[.][/]//g' | sed -e 's/[.].*//g'`
+    
+    tmp="tmp"
+    
+        
+    mkdir $tmp
+    unzip $f -d "tmp" &> /dev/null
+    #read zipnote to file, removing lines that hold unnecceary info.
+    zipnote $f |sed 's/^@.*$//'| sed '/^$/d'> $tmp"/conf"
+    
+    mv $tmp"/BookText" $base".txt"
+    mv $tmp"/SearchDB" $base".TDB"
+    mv $tmp"/LevelMap" $base".LMP"
+    mv $tmp"/conf" $base".conf" 
+    
+    #cleanup:
+    
+#    when you're sure it works:
+    rm $f
+    
+    rm -r $tmp
+done
