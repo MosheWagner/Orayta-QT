5390e046be6a030825def8f2dc5a42f7579fa2bc
yoch.melka@gmail.com <yoch.melka@gmail.com@aeb61573-d5a9-0769-0c8e-f8bf28dd2c43>
2012-07-30 09:08:20 +0000

    add script for convert .txt files to .obk files (without SearchDB)

---
+++ BooksSrc/script3.bat
@@ -0,0 +1,7 @@
+REM create .obk files from .txt
+REM for windows only. call it from cmd.exe
+REM this script use recent version of 7zip
+FOR /r %%F IN (*.txt) DO (
+"C:\Program Files\7-Zip\7z.exe" a -tzip %%~pnF.obk %%F
+"C:\Program Files\7-Zip\7z.exe" rn -tzip %%~pnF.obk %%~nxF BookText 
+)
