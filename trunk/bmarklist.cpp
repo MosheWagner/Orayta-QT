41c9489acc0facbf49204c53f39c24127144b634
izar00@gmail.com <izar00@gmail.com@aeb61573-d5a9-0769-0c8e-f8bf28dd2c43>
2012-08-10 14:01:22 +0000

    עדכון לגירסת אנדרואיד, תיקונים למערכת עדכון ספרים ולמערכת דף יומי

--- trunk/bmarklist.cpp
+++ trunk/bmarklist.cpp
@@ -24,14 +24,21 @@ void BMarkList::loadHistory(BookList booklist){
     }
     settings.endArray();
 
-//blaTODO: read history to list.
+    //TODO: let the user decide these options.
+    dafYomiActive = true;
+    parshaYomiActive = false;
+    mishnaYomiActive = false;
+
+    if (dafYomiActive){
+        addDafYomi(booklist);
+    }
 }
 
 
 BMarkList::~BMarkList(){
     QSettings settings("Orayta", "SingleUser");
 
-    //reload bookmark list
+    //save book mark list to memory
     settings.beginWriteArray("BookMarkList");
     const QList<QListWidgetItem*> list =  findItems("", Qt::MatchContains);
     int id = 1;
@@ -72,4 +79,36 @@ void BMarkList::newBookMark(Book* book, BookIter iter)
     MiniBMark* bm = new MiniBMark(book, iter);
 //    addItem(bm);
     insertItem(0, bm);
+}
+
+bool BMarkList::isDafYomiActive() {return dafYomiActive;}
+bool BMarkList::isParshaYomiActive(){return parshaYomiActive;}
+bool BMarkList::isMishnaYomiActive(){return mishnaYomiActive;}
+
+void BMarkList::setDafYomiActive(bool b) {dafYomiActive = b;}
+void BMarkList::setParshaYomiActive(bool b) {parshaYomiActive = b;}
+void BMarkList::setMishnaYomiActive(bool b) {mishnaYomiActive = b;}
+
+void BMarkList::addDafYomi(BookList booklist) {
+
+    //get string representation of current day yomi:
+    QString dyFull = dafYomi(QDate::currentDate ());
+
+    QChar separator = '%';
+    //get masechet and daf separatly:
+    QString dyMasechet = dyFull.split(separator).at(0);
+    QString dyDaf = dyFull.split(separator).at(1);
+
+    // find book object from string
+    Book* dyBook = booklist.FindBookByName(dyMasechet);
+    //if we cant find the book
+    if (!dyBook) return;
+
+    // convert the string to bookIter
+    BookIter iter;
+    //TODO: this is only a dummy method, it wont work!
+    //MOSHE please fix soon!@#$%^^&&*()!
+    iter.fromString(dyDaf);
+
+    newBookMark(dyBook, iter);
 }
